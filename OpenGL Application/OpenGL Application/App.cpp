9981d5dad7687eacfbd81a6b8190b53cad975484
DESKTOPPY\Max <mmowbray@mentlegen.com>
2016-12-06 20:55:55 -0500

    Add loadShaders(...) functiion with skeleton vertex and fragment shaders

--- OpenGL Application/OpenGL Application/App.cpp
+++ OpenGL Application/OpenGL Application/App.cpp
@@ -1,15 +1,112 @@
 #include "glew.h"
-#include "glfw3.h"	
+#include "glfw3.h"
+
+#include <vector>
+#include <string>
+#include <fstream>
+
+GLuint loadShaders(std::string vertex_shader_path, std::string fragment_shader_path)
+{
+	// Create the shaders
+	GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+
+	// Read the Vertex Shader code from the file
+	std::string VertexShaderCode;
+	std::ifstream VertexShaderStream(vertex_shader_path, std::ios::in);
+	if (VertexShaderStream.is_open()) {
+		std::string Line = "";
+		while (getline(VertexShaderStream, Line))
+			VertexShaderCode += "\n" + Line;
+		VertexShaderStream.close();
+	}
+	else {
+		printf("Impossible to open %s. Are you in the right directory?\n", vertex_shader_path.c_str());
+		getchar();
+		exit(-1);
+	}
+
+	// Read the Fragment Shader code from the file
+	std::string FragmentShaderCode;
+	std::ifstream FragmentShaderStream(fragment_shader_path, std::ios::in);
+	if (FragmentShaderStream.is_open()) {
+		std::string Line = "";
+		while (getline(FragmentShaderStream, Line))
+			FragmentShaderCode += "\n" + Line;
+		FragmentShaderStream.close();
+	}
+	else {
+		printf("Impossible to open %s. Are you in the right directory?\n", fragment_shader_path.c_str());
+		getchar();
+		exit(-1);
+	}
+
+	GLint Result = GL_FALSE;
+	int InfoLogLength;
+
+	// Compile Vertex Shader
+	printf("Compiling shader : %s\n", vertex_shader_path.c_str());
+	char const * VertexSourcePointer = VertexShaderCode.c_str();
+	glShaderSource(VertexShaderID, 1, &VertexSourcePointer, nullptr);
+	glCompileShader(VertexShaderID);
+
+	// Check Vertex Shader
+	glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
+	glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if (InfoLogLength > 0) {
+		std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
+		glGetShaderInfoLog(VertexShaderID, InfoLogLength, nullptr, &VertexShaderErrorMessage[0]);
+		printf("%s\n", &VertexShaderErrorMessage[0]);
+	}
+
+	// Compile Fragment Shader
+	printf("Compiling shader : %s\n", fragment_shader_path.c_str());
+	char const * FragmentSourcePointer = FragmentShaderCode.c_str();
+	glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, nullptr);
+	glCompileShader(FragmentShaderID);
+
+	// Check Fragment Shader
+	glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
+	glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if (InfoLogLength > 0) {
+		std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
+		glGetShaderInfoLog(FragmentShaderID, InfoLogLength, nullptr, &FragmentShaderErrorMessage[0]);
+		printf("%s\n", &FragmentShaderErrorMessage[0]);
+	}
+
+	// Link the program
+	printf("Linking program\n");
+	GLuint program_id = glCreateProgram();
+	glAttachShader(program_id, VertexShaderID);
+	glAttachShader(program_id, FragmentShaderID);
+
+	glBindAttribLocation(program_id, 0, "in_Position");
+	glBindFragDataLocation(program_id, 0, "out_Color");
+
+	glLinkProgram(program_id);
+
+	// Check the program
+	glGetProgramiv(program_id, GL_LINK_STATUS, &Result);
+	glGetProgramiv(program_id, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if (InfoLogLength > 0) {
+		std::vector<char> ProgramErrorMessage(InfoLogLength + 1);
+		glGetProgramInfoLog(program_id, InfoLogLength, nullptr, &ProgramErrorMessage[0]);
+		printf("%s\n", &ProgramErrorMessage[0]);
+	}
+
+	glDetachShader(program_id, VertexShaderID);
+	glDetachShader(program_id, FragmentShaderID);
+
+	glDeleteShader(VertexShaderID);
+	glDeleteShader(FragmentShaderID);
+
+	return program_id;
+}
 
 int main()
 {
-	glewExperimental = GL_TRUE;
-	glewInit();
 
 	GLFWwindow* window;
-
-	if (!glewInit())
-		return -1;
 
 	if (!glfwInit())
 		return -1;
@@ -22,6 +119,16 @@ int main()
 	}
 
 	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		return -1;
+	}
+
+	GLuint shader_program = loadShaders("triangle.vs", "triangle.fs");
+	glUseProgram(shader_program);
 
 	while (!glfwWindowShouldClose(window))
 	{
---
+++ OpenGL Application/OpenGL Application/triangle.fs
@@ -0,0 +1,7 @@
+#version 330
+
+out vec4 frag_colour;
+
+void main () {
+	frag_colour = vec4(1.0f, 0.0f, 1.0f, 1.0f);
+};
