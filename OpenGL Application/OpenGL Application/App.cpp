3b98bd74e8112d565beafe9e079b3afe6388f7b2
DESKTOPPY\Max <mmowbray@mentlegen.com>
2016-12-06 20:28:54 -0500

    Add statically-linked GLFW3 for both Debug and Release 32-bit build configurations

--- OpenGL Application/OpenGL Application/App.cpp
+++ OpenGL Application/OpenGL Application/App.cpp
@@ -1,12 +1,37 @@
-#include <iostream>
-
 #include "glew.h"
+#include "glfw3.h"	
 
 int main()
 {
 	glewExperimental = GL_TRUE;
 	glewInit();
 
-	std::cout << "Hello World!" << std::endl;
-	getchar();
+	GLFWwindow* window;
+
+	if (!glewInit())
+		return -1;
+
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
